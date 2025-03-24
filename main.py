711c8a216d85511582ac7e551d1aee2f9f16430c
alenaxromova <59444293+alenaxromova@users.noreply.github.com>
2025-03-25 02:26:50 +0600

    main.py

--- main.py
+++ main.py
@@ -69,3 +69,5 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render передаёт PORT из переменной окружения
     app.run(host="0.0.0.0", port=port)
+
+# перезапуск
